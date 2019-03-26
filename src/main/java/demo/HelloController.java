0bbf4a080093b16060e07eeb6b1e4c9e023a109f
Koby Kilimnik <koby.kilimnik@imperva.com>
2019-03-26 16:13:38 +0200

    added support for index default path with param name

--- src/main/java/demo/HelloController.java
+++ src/main/java/demo/HelloController.java
@@ -3,9 +3,16 @@ package demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
+    @GetMapping("/")
+    public String hello(@RequestParam(name = "name", required = false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 
     @GetMapping("/blah")
     public String blah(Model model) {
